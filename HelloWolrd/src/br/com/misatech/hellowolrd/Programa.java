88953a41af056a1346b730b1d8e81512087a55e4
Misael da Costa Homem <misatech@ig.com.br>
2014-03-04 06:59:29 -0300

    Refatoração de código.

--- HelloWolrd/src/br/com/misatech/hellowolrd/Programa.java
+++ HelloWolrd/src/br/com/misatech/hellowolrd/Programa.java
@@ -1,8 +1,27 @@
 package br.com.misatech.hellowolrd;
 
+/**
+ * A classe possui um método que exibe uma mensagem de saudação inicial no console. 
+ * 
+ * @author Misael C. Homem
+ * @since Versão 1.00
+ */
 public class Programa {
 
+	/**
+	 * Método principal da classe.
+	 * @param args argumento opcional de linha de comando.
+	 */
 	public static void main(String []args) {
+		
+		new Programa().initialMessage();
+		
+	}
+	
+	/**
+	 * Exibe uma mensagem initial no console.
+	 */
+	public void initialMessage() {
 		
 		System.out.println("Olá mundo, estou no GitHub");
 		
