8dc8e5ad5df53ac2413b2100eb34458c5d51233a
Misael da Costa Homem <misatech@ig.com.br>
2014-03-14 23:35:14 -0300

    Iniciando projeto ListaTelefonica conforme documentação Alg3_N_B_T1.pdf

---
+++ ListaTelefonica/src/br/com/misatech/listatelefonica/Programa.java
@@ -0,0 +1,20 @@
+package br.com.misatech.listatelefonica;
+
+/**
+ * Classe contendo o método principal para execução do programa.
+ * @author Misael C. Homem.
+ * @since 1.00
+ */
+public class Programa {
+
+	/**
+	 * Método principal da aplicação.
+	 * @param args argumento de linha de comando.
+	 */
+	public static void main(String []args) {
+		
+		
+		
+	}
+	
+}
